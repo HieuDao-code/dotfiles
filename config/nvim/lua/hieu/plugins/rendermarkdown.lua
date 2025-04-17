a992e6ffef606f0c7b677362e3555bf154710517
HieuDao-code <106801182+HieuDao-code@users.noreply.github.com>
2025-04-17 20:19:30 +0200

    Update rendermarkdown.lua

--- config/nvim/lua/hieu/plugins/rendermarkdown.lua
+++ config/nvim/lua/hieu/plugins/rendermarkdown.lua
@@ -4,5 +4,9 @@ return {
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
-    opts = {},
+    config = function()
+require('render-markdown').setup({
+    completions = { blink = { enabled = true } },
+})
+end
 }
