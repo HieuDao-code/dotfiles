8b06b6b701cabaaab84639b7c2c0c9990908de02
HieuDao-code <106801182+HieuDao-code@users.noreply.github.com>
2025-04-18 19:35:38 +0200

    Update mini.lua

--- config/nvim/lua/hieu/plugins/mini.lua
+++ config/nvim/lua/hieu/plugins/mini.lua
@@ -25,13 +25,6 @@ return {
     -- set use_icons to true if you have a Nerd Font
     statusline.setup { use_icons = vim.g.have_nerd_font }
 
-    -- You can configure sections in the statusline by overriding their
-    -- default behavior. For example, here we set the section for
-    -- cursor location to LINE:COLUMN
-    statusline.section_location = function()
-      return '%2l:%-2v'
-    end
-
     -- Add/delete/replace surroundings (brackets, quotes, etc.)
     require('mini.surround').setup {
       mappings = {
