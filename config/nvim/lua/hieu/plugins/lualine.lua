c1e1483b06298ebc67ff5bbedc85287a4e50f24f
HieuDao-code <106801182+HieuDao-code@users.noreply.github.com>
2025-04-21 06:20:04 +0200

    Update lualine.lua

--- config/nvim/lua/hieu/plugins/lualine.lua
+++ config/nvim/lua/hieu/plugins/lualine.lua
@@ -2,6 +2,10 @@
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
 opts = {
-options = { theme = 'gruvbox-material' }
+options = { theme = 'gruvbox-material',
+section_separators = { left = '', right = '' },
+  component_separators = { left = '', right = '' }
+}
+
 }
 }
