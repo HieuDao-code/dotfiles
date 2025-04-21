3fbdfc9d419a63db0d36dd3a1839175a97d3b8d2
HieuDao-code <106801182+HieuDao-code@users.noreply.github.com>
2025-04-21 05:21:23 +0200

    Update mini.lua

--- config/nvim/lua/hieu/plugins/mini.lua
+++ config/nvim/lua/hieu/plugins/mini.lua
@@ -21,9 +21,9 @@ return {
     require('mini.icons').setup()
 
     -- Simple and easy statusline.
-    local statusline = require 'mini.statusline'
+    -- local statusline = require 'mini.statusline'
     -- set use_icons to true if you have a Nerd Font
-    statusline.setup { use_icons = vim.g.have_nerd_font }
+    -- statusline.setup { use_icons = vim.g.have_nerd_font }
 
 
     -- Snippets Engine
