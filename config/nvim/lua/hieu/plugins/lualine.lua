f23584d58738992488b97bc6b2e45bbddb9ab3e3
HieuDao-code <106801182+HieuDao-code@users.noreply.github.com>
2025-04-21 06:56:24 +0200

    Update lualine.lua

--- config/nvim/lua/hieu/plugins/lualine.lua
+++ config/nvim/lua/hieu/plugins/lualine.lua
@@ -6,6 +6,14 @@
       } },
 opts = {
 options = { theme = 'gruvbox-material',
+sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename', 'harpoon2'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
 section_separators = { left = '\ue0b0', right = '\ue0b2' },
   component_separators = { left = '\ue0b0', right = '\ue0b2' }
 }
