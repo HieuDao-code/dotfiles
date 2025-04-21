7b155c6d2563e201e019c3caf552eb336a355c86
HieuDao-code <106801182+HieuDao-code@users.noreply.github.com>
2025-04-21 06:46:05 +0200

    Update lualine.lua

--- config/nvim/lua/hieu/plugins/lualine.lua
+++ config/nvim/lua/hieu/plugins/lualine.lua
@@ -1,6 +1,9 @@
 {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    dependencies = { 'nvim-tree/nvim-web-devicons', {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+      } },
 opts = {
 options = { theme = 'gruvbox-material',
 section_separators = { left = '\ue0b0', right = '\ue0b2' },
