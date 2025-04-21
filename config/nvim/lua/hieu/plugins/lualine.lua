6c50eb29a5fa79dbd6d5dcc94dc4877578cd8930
HieuDao-code <106801182+HieuDao-code@users.noreply.github.com>
2025-04-21 20:12:57 +0200

    Update lualine.lua

--- config/nvim/lua/hieu/plugins/lualine.lua
+++ config/nvim/lua/hieu/plugins/lualine.lua
@@ -1,19 +1,24 @@
 {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons', {
+    dependencies = { 'nvim-tree/nvim-web-devicons',   {
+    "letieu/harpoon-lualine",
+    dependencies = {
+      {
         "ThePrimeagen/harpoon",
         branch = "harpoon2",
-      } },
+      }
+    },
+  } },
 opts = {
 options = { theme = 'gruvbox-material',
 sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
     lualine_c = {'filename' },
-    lualine_x = {
+    lualine_x = {'%=', {
             'harpoon2',
             indicators = { '1', '2', '3', '4', '5' },
-            active_indicators = { '[1]', '[2]', '[3]', '[4]', '[5]' },
+            active_indicators = { '[1]', '[2]', '[3]', '[4]', '[5]' }},
           },
     lualine_y = {'encoding', 'fileformat', 'filetype'},
     lualine_z = {'location'}
