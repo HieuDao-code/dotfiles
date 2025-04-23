382e77ea42dc492501edcefc42b5a0455316aae7
HieuDao-code <106801182+HieuDao-code@users.noreply.github.com>
2025-04-23 22:29:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 
+# Echo's the operating system, simplified to:
+# - osx
+# - ubuntu
+get_os() {
+    # Identify the operating system.
+    local un=$(uname -a)
+    os="unknown"
+    if [[ "$un" =~ [Dd]arwin ]]; then
+        echo "osx"
+    elif [[ "$un" =~ [Uu]buntu ]]; then
+        echo "ubuntu"
+    else
+        logger -s "Unable to idenfify operating system from uname '$un'"
+        exit 1
+    fi
+}
+
+# Get the operating system, output it. The script will terminate if the OS
+# cannot be categorically identified.
+os=$(get_os)
+echo "os identified as: $os
+
 # Install all packages
 
 
