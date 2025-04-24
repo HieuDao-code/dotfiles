61d23b339c9c7a866c1fa23ec14808dabb1ec851
HieuDao-code <106801182+HieuDao-code@users.noreply.github.com>
2025-04-24 19:49:06 +0200

    Update 06_window_manager.sh

--- install/06_window_manager.sh
+++ install/06_window_manager.sh
@@ -2,7 +2,13 @@
 if ask "$os: Install/Update aerospace and skethybar?" Y; then
     if [[ "$os" == "osx" ]]; then
         echo "$os: Installing Aerospace..."
+        brew install --cask nikitabobko/tap/aerospace
 
+        # Move windows by holding ctrl + cmd and dragging any part of the window (not necessarily the window title)
+        defaults write -g NSWindowShouldDragOnGesture -bool true
+
+        # Disable windows opening animations
+        defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
         echo "$os: Installing Sketchybar..."
 
     elif [[ "$os" == "ubuntu" ]]; then
