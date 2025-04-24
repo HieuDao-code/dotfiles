6d38fb1f0d7bb69aa8f0e320f8c3fd1e0444e7af
HieuDao-code <106801182+HieuDao-code@users.noreply.github.com>
2025-04-24 19:52:40 +0200

    Update 04_tmux.sh

--- install/04_tmux.sh
+++ install/04_tmux.sh
@@ -3,6 +3,7 @@ echo "$os: Updating tmux..."
 if ask "$os: Install/Update tmux?" Y; then
     if [[ "$os" == "osx" ]]; then
         brew install tmux
+
     elif [[ "$os" == "ubuntu" ]]; then
         sudo apt install -y tmux
     fi
