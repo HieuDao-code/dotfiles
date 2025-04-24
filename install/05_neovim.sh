677567c9efec3adeb46a8a69f882f6ef89dc6f9d
HieuDao-code <106801182+HieuDao-code@users.noreply.github.com>
2025-04-24 19:51:52 +0200

    Update 05_neovim.sh

--- install/05_neovim.sh
+++ install/05_neovim.sh
@@ -4,6 +4,7 @@ if ask "$os: Install/Update tmux?" Y; then
     if [[ "$os" == "osx" ]]; then
         brew install neovim
         brew install ripgrep fd
+
     elif [[ "$os" == "ubuntu" ]]; then
         sudo apt install -y neovim
         sudo apt install -y make gcc ripgrep unzip xclip, fd-find
