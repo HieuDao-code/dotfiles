f35399569d28a7a70f82cf9ef327f0abad99954d
HieuDao-code <106801182+HieuDao-code@users.noreply.github.com>
2025-04-24 12:53:11 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,8 +22,17 @@ get_os() {
 os=$(get_os)
 echo "os identified as: $os
 
-# Install all packages
+# Run each of the setup files.
+for file in ./install/*; do
+    # If we don't have a file (this happens when we find no results), then just
+    # move onto the next file (or finish the loop).
+    [ -e "$file" ] || continue
 
+    # Ask the user if they want to setup the feature, then setup or skip.
+    feature=$(basename "$file")
+    if ! ask "$os: setup feature '$feature'?" "N"; then continue; fi
+    source $file
+done
 
 # Install Stow
 echo "Installing Stow..."
