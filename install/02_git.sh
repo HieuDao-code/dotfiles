003215331e5284efc0ef11516e7c8cff8f8ba2ab
HieuDao-code <106801182+HieuDao-code@users.noreply.github.com>
2025-04-24 13:20:05 +0200

    Update 02_git.sh

--- install/02_git.sh
+++ install/02_git.sh
@@ -1,9 +1,8 @@
 # Configure Git.
+echo "$os: Installing git..."
 if [[ "$os" == "osx" ]]; then
-    echo "$os: Installing git..."
     brew install git
 
 elif [[ "$os" == "ubuntu" ]]; then
-    echo "$os: Installing git..."
     sudo apt install -y git
 fi
