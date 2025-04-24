76433ec993b3db94315ad42585fd9f71117bc66c
HieuDao-code <106801182+HieuDao-code@users.noreply.github.com>
2025-04-24 19:44:06 +0200

    Update 06_window_manager.sh

--- install/06_window_manager.sh
+++ install/06_window_manager.sh
@@ -1,1 +1,12 @@
-test
+# Install window manager
+if ask "$os: Install/Update aerospace and skethybar?" Y; then
+    if [[ "$os" == "osx" ]]; then
+        echo "$os: Installing Aerospace..."
+
+        echo "$os: Installing Sketchybar..."
+
+    elif [[ "$os" == "ubuntu" ]]; then
+        echo "$os: Installing i3..."
+
+    fi
+fi
