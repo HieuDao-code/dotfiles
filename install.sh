70037374caf32ed1124660b5835a9bf28263ae96
HieuDao-code <106801182+HieuDao-code@users.noreply.github.com>
2025-04-24 12:54:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,7 +35,7 @@ for file in ./install/*; do
 done
 
 # Install Stow
-echo "Installing Stow..."
+echo "$os: installing Stow..."
 if [[ "$os" == "osx" ]]; then
     brew install stow
 if [[ "$os" == "ubuntu" ]]; then
