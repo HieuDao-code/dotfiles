1a67d09729ac1e7a683424ccf9b744ec1810bb44
HieuDao-code <106801182+HieuDao-code@users.noreply.github.com>
2025-04-24 13:24:12 +0200

    Update 03_alacritty.sh

--- install/03_alacritty.sh
+++ install/03_alacritty.sh
@@ -1,7 +1,11 @@
 # Install Alacritty
 echo "$os: Installing alacritty..."
 if [[ "$os" == "osx" ]]; then
-    brew install git
+    sudo snap install alacritty --classic
+
+    echo "$os: Set Alacritty as default terminal..."
+    sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator $(which alacritty) 50
+    sudo update-alternatives --config x-terminal-emulator
 
 elif [[ "$os" == "ubuntu" ]]; then
     sudo apt install -y git
