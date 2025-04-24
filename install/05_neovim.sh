200138fa46a46d2cd6fec763c2184c3f9bf386f1
HieuDao-code <106801182+HieuDao-code@users.noreply.github.com>
2025-04-24 15:21:29 +0200

    Update 05_neovim.sh

--- install/05_neovim.sh
+++ install/05_neovim.sh
@@ -1,1 +1,9 @@
-todo
+# Install Neovim
+echo "$os: Updating Neovim..."
+if ask "$os: Install/Update tmux?" Y; then
+    if [[ "$os" == "osx" ]]; then
+        brew install tmux
+    elif [[ "$os" == "ubuntu" ]]; then
+        sudo apt install -y tmux
+    fi
+fi
