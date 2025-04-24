4440afb38902b3f835bb786417da1b1c55533c17
HieuDao-code <106801182+HieuDao-code@users.noreply.github.com>
2025-04-24 12:55:55 +0200

    Create install/01_package_manager.sh

---
+++ install/01_package_manager.sh
@@ -0,0 +1,40 @@
+# Setup any package manager required.
+if [[ "$os" == "osx" ]]; then
+    echo "$os: Checking for brew..."
+    which -s brew
+    if [[ $? != 0 ]] ; then
+        if ask "$os: HomeBrew is not installed. Install it?" Y; then
+            echo "$os: Installing HomeBrew..."
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+            echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> $HOME/.zprofile
+            # If there was an error, it might be this issue:
+            echo "If you see an error above, run:"
+            echo "  brew doctor"
+            echo "You will need to restart your shell now:"
+            echo "  exec -l $SHELL"
+            exit 0
+        fi
+    fi
+    if ask "$os: Update HomeBrew?" Y; then
+        echo "$os: Updating brew..."
+        brew update
+    fi
+elif [[ "$os" == "ubuntu" ]]; then
+    if ask "$os: update apt?" Y; then
+        echo "$os: Updating apt..."
+        sudo apt-get update -y
+    fi
+
+    # Ensure that snap is installed for Ubuntu.
+    if [ ! -x "$(command -v snap)" ]; then
+        if ask "$os: 'snap' is required for $os, install now?" Y; then
+            echo "$os: Installing snap..."
+            sudo apt-get install -y snapd
+        else
+            echo "$os: Exiting setup as 'snap' is required...";
+            return 0;
+        fi
+    else
+        echo "$os: 'snap' is installed..."
+    fi
+fi
