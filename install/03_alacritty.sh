04629a30a1e2362396a1ba202f59a7164697864f
HieuDao-code <106801182+HieuDao-code@users.noreply.github.com>
2025-04-24 13:20:39 +0200

    Update 03_alacritty.sh

--- install/03_alacritty.sh
+++ install/03_alacritty.sh
@@ -1,1 +1,8 @@
-test
+# Install Alacritty
+echo "$os: Installing alacritty..."
+if [[ "$os" == "osx" ]]; then
+    brew install git
+
+elif [[ "$os" == "ubuntu" ]]; then
+    sudo apt install -y git
+fi
