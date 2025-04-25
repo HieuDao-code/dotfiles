d6173ad25ee02014b31bf200148ae9e9e27d4ddc
HieuDao-code <106801182+HieuDao-code@users.noreply.github.com>
2025-04-25 06:45:01 +0200

    Update 08_python.sh

--- install/08_python.sh
+++ install/08_python.sh
@@ -1,1 +1,15 @@
 # Install basic python tooling
+if ask "$os: Install python tooling?" Y; then
+    if [[ "$os" == "osx" ]]; then
+        brew install uv
+
+    elif [[ "$os" == "ubuntu" ]]; then
+        pipx install uv
+
+    fi
+
+    echo "$os: Add shell completion for UV and UV x..."
+    echo 'eval "$(uv generate-shell-completion zsh)"' >> ~/.zshrc
+   echo 'eval "$(uvx --generate-shell-completion zsh)"' >> ~/.zshrc 
+    
+fi
