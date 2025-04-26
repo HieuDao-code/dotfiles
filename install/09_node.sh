e04016d0428da608de121bb740df7ccd396bdc87
Your Name <hieu.dao96@gmail.com>
2025-04-26 19:49:05 +0200

    oil show git tracked hidden files

--- install/09_node.sh
+++ install/09_node.sh
@@ -1,11 +1,8 @@
-# If NVM (Node version manager) is not installed, install it.
-if ask "$os: Install node version manager?" N; then
+# Install NVM (Node version manager)
+if ask "$os: Install node version manager and latest node lts?" N; then
     echo "$os: Installing NVM..."
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-fi
 
-# Install the current node lts.
-if ask "$os: Install and use node lts as default?" N; then
     export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
     nvm install --lts --default
