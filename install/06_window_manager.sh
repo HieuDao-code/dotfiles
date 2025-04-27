884b2d7146b1f747fbb2827650a6b86027cb260c
Your Name <hieu.dao96@gmail.com>
2025-04-27 19:50:20 +0200

    configs

---
+++ install/06_window_manager.sh
@@ -0,0 +1,19 @@
+# Install window manager
+if [[ "$os" == "osx" ]]; then
+    echo "$os: Installing Aerospace..."
+    brew install --cask nikitabobko/tap/aerospace
+
+    # Move windows by holding ctrl + cmd and dragging any part of the window (not necessarily the window title)
+    defaults write -g NSWindowShouldDragOnGesture -bool true
+
+    # Disable windows opening animations
+    defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
+
+    echo "$os: Installing Sketchybar..."
+    brew tap FelixKratz/formulae
+    brew install sketchybar
+
+elif [[ "$os" == "ubuntu" ]]; then
+    echo "$os: Installing i3..."
+    sudo apt install i3
+fi
---
+++ install/07_python.sh
@@ -0,0 +1,16 @@
+# Install basic python tooling
+echo "$os: Installing uv..."
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+echo "$os: Install python tooling..."
+
+echo "$os: Install commitizen..."
+uv tool install commitizen
+echo "$os: Install mkdocs-material..."
+uv tool install mkdocs --with mkdocs-material
+echo "$os: Install pre-commit..."
+uv tool install pre-commit
+echo "$os: Install ruff..."
+uv tool install ruff
+echo "$os: Install tox..."
+uv tool install tox
